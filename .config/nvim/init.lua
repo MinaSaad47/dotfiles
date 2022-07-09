52630de72883b636ac99752067ca382410c4549c
MinaSaad47 <MinaSaad-47@outlook.com>
2022-07-09 12:32:48 +0200

    [nvim] check if opened from vscodium

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,7 @@
-require('settings')
-require('mappings')
-require('packer-conf')
-require('colorscheme-conf')
-require('plugins-conf')
+if vim.fn.exists("g:vscode") == 0 then
+    require('settings')
+    require('mappings')
+    require('packer-conf')
+    require('colorscheme-conf')
+    require('plugins-conf')
+end
