7f5a210ba2b60dfe62e9e1ac628262d6e8df4099
MinaSaad47 <MinaSaad47@outlook.com>
2022-09-16 09:31:19 +0200

    only needed treesitters langs

--- .config/nvim/lua/plugins-conf/nvim-treesetter/init.lua
+++ .config/nvim/lua/plugins-conf/nvim-treesetter/init.lua
@@ -5,7 +5,7 @@ end
 
 treesitter.setup {
   -- A list of parser names, or "all"
-  ensure_installed = 'all',
+  ensure_installed = { 'rust', 'go', 'c', 'cpp' },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
