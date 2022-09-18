496ced389c4a50cc7ce0247414eca880cdda8fee
MinaSaad47 <MinaSaad-47@outlook.com>
2022-09-18 17:45:04 +0200

    [neovim] fixed error

--- .config/nvim/lua/settings/init.lua
+++ .config/nvim/lua/settings/init.lua
@@ -25,12 +25,7 @@ set.cursorline = true
 
 set.hidden = true
 
-<<<<<<< Updated upstream
 set.cmdheight = 1
-||||||| Stash base
-set.cmdheight = 0
-=======
->>>>>>> Stashed changes
 set.laststatus = 3
 
 set.mouse='a'
