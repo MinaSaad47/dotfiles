b2c15a286f732427461aa0e303053791181a9ea4
MinaSaad47 <MinaSaad-47@outlook.com>
2022-10-18 17:59:58 +0200

    [neovim]

--- .config/nvim/lua/plugins-conf/nvim-lspconfig/init.lua
+++ .config/nvim/lua/plugins-conf/nvim-lspconfig/init.lua
@@ -62,7 +62,7 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 local status_ok, cmp_nvim_lua = require('cmp_nvim_lsp')
 if status_ok then
-    capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+    capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 end
 
 local opts = {
