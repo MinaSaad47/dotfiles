72f17b461fd08aee011f490d304e5d5044975303
MinaSaad47 <MinaSaad-47@outlook.com>
2022-10-19 12:12:36 +0200

    [neovim] colorscheme

--- .config/nvim/lua/colorscheme-conf/init.lua
+++ .config/nvim/lua/colorscheme-conf/init.lua
@@ -1,12 +1,25 @@
 local colorscheme = "gruvbox"
-
-
-vim.g['gruvbox_contrast_dark'] = 'hard'
-vim.opt.background = "dark"
-
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
+vim.o.background = "dark"
+local status, gruvbox =  pcall(require, colorscheme)
+if status then
+    gruvbox.setup({
+      undercurl = true,
+      underline = true,
+      bold = true,
+      italic = true,
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      invert_intend_guides = false,
+      inverse = false, -- invert background for search, diffs, statuslines and errors
+      contrast = "hard", -- can be "hard", "soft" or empty string
+      palette_overrides = {},
+      overrides = {},
+      dim_inactive = false,
+      transparent_mode = false,
+    })
+    vim.cmd("colorscheme" .. " " .. colorscheme)
+else
   vim.notify("colorscheme " .. colorscheme .. " not found!")
-  return
 end
-
